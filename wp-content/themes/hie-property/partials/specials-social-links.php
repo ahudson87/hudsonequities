844c8245b8410842132e622ef977895f387d9fd2
Alex Hudson <ahudson@alexhudsondesign.com>
2018-11-27 22:09:22 -0800

    More debugging LinkedIn Specials sharing

--- wp-content/themes/hie-property/partials/specials-social-links.php
+++ wp-content/themes/hie-property/partials/specials-social-links.php
@@ -1,5 +1,5 @@
 <div class="social-icons">
     <span class="social-share-text">Share this special</span>
     <a class="social-icon social-icon-facebook" href="http://www.facebook.com/sharer.php?u=<?php echo get_permalink(); ?>" target="_blank"></a>
-    <a class="social-icon social-icon-linkedin" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>&title=<?php echo get_sub_field('special_title'); ?>&summary=See Our Specials&source=<?php echo get_site_url(); ?>" target="_blank"></a>
+    <a class="social-icon social-icon-linkedin" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>&source=<?php echo get_site_url(); ?>" target="_blank"></a>
 </div>
